c1ea62ecacfdc81bcb3658aee1b34fa82c733052
DangLamTung <danglamtung199@gmail.com>
2021-05-27 12:36:09 +0700

    Add files via upload

--- Software/Segmentation+Occumap/python_gps.py
+++ Software/Segmentation+Occumap/python_gps.py
@@ -4,17 +4,15 @@ import pynmea2
 import serial
 
 
-ser = serial.Serial('COM10', 115200, timeout=5.0)
+ser = serial.Serial('COM17', 9600, timeout=5.0)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
 while 1:
     try:
         line = sio.readline()
         msg = pynmea2.parse(line)
-        print(repr(msg))
-    except serial.SerialException as e:
-        print('Device error: {}'.format(e))
-        break
-    except pynmea2.ParseError as e:
+        print(msg.latitude,msg.longitude)
+
+    except Exception as e:
         print('Parse error: {}'.format(e))
         continue
